f05a1e71db6fea30f0c65621e78e5fc41938da59
jihye kang <jihye59kang@gmail.com>
2021-07-21 13:17:36 +0100

    language change function

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class App extends React.Component{
+
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language })
+    }
+
     render() {
         return (
             <div>
-                App
+                <div>
+                    Select a language
+                    <div onClick={() => this.onLanguageChange('english')}>english</div>
+                    <div onClick={() => this.onLanguageChange('dutch')}>dutch</div>
+                </div>
+                <div>
+                    {this.state.language}
+                </div>
             </div>
         )
     }
